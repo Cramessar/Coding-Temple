ebdb11f51733a537c5725f5d1d45789b75e07b1f
Cramessar <154475437+Cramessar@users.noreply.github.com>
2024-10-22 17:50:31 -0400

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,1 +1,21 @@
+#in the unlikely event someone actually reviews this I am totally joking about using gpt.
 print("hello world")
+
+hobbies = ["sleeping", "watching youtube", "coding", "gaming", "cheating on assignments using gpt"]
+
+print("My favorite hobbies are:")
+for hobby in hobbies:
+    print(hobby)
+
+hobbies.append("cheating on assignments using gpt")
+print("\nAdded 'cheating on assignments using gpt' to the list of hobbies:")
+
+hobbies.remove("coding")
+print("\nRemoved 'coding' from the list of hobbies:")
+
+
+hobbies.sort()
+print("\nSorted list of hobbies alphabetically:")
+
+for hobby in hobbies:
+    print(hobby)
