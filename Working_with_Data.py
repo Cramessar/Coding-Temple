caeb00cd6f2a208e2dc1b3d2de1d63da80fac305
Cramessar <154475437+Cramessar@users.noreply.github.com>
2024-10-22 18:18:01 -0400

    Update Working_with_Data.py

--- Working_with_Data.py
+++ Working_with_Data.py
@@ -1,17 +1,20 @@
 #again if anyone reads this code dont judge me too harshly
+
+#numbers and squares
 numbers = list(range(0, 10))
 print("list of numbers", numbers)
 squares = [number**2 for number in numbers]
 print("List of squares:", squares)
 
 
-
+#odd or even
 number = int(input("Enter a number: "))
 if number % 2 == 0:
     print(f"{number} is an even number.")
 else:
     print(f"{number} is an odd number.")
-    
+ 
+#pizza toppings, yes pineapple is acceptable!    
 toppings = []
 while True:
     topping = input("Enter a pizza topping (type 'quit' to finish): ")
