fd3a23b872318c7cddce1f596c3191e28b041859
Cramessar <154475437+Cramessar@users.noreply.github.com>
2024-11-03 23:43:09 -0500

    saves

---
+++ Module1/lesson2.py
@@ -0,0 +1,5 @@
+#Lesson2
+
+name=(input("What is your name?"))
+age=int(input("How old are you?"))
+print("my name is {} and I am {} years old".format(name,age))
---
+++ Module1/lesson3.py
@@ -0,0 +1,6 @@
+import random
+
+#lesson3
+firstname = ["Chris","Mia","Cloud","Luna"]
+lastname = ["Ramessar","Franklyn","Totaram"]
+print(random.choice(firstname),(random.choice(lastname)))
