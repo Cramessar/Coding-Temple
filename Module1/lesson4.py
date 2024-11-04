7b084c011f6b927533e0c8ab7a8904d9cf792025
Cramessar <154475437+Cramessar@users.noreply.github.com>
2024-11-03 23:42:29 -0500

    changed file structure

---
+++ Module1/lesson4.py
@@ -0,0 +1,37 @@
+import random
+
+#lesson4
+# print("this is you calculator, its simple but powerful")
+# print("select your first number.")
+# a=int(input())
+# print("select your second number")
+# b=int(input())
+
+# print("Choose an operation")
+# print("1. add")
+# print("2. subtract")
+# print("3. multiply")
+# print("4. divide")
+# print("Choose 1-4.")
+
+# choice =  input()
+# if choice == "1":
+#     print(("the sum is,"),(a+b))
+# if choice == "2":
+#     print(("the difference is,"),(a-b))
+# if choice == "3":
+#     print(("your product is,"),(a*b))
+# if choice == "4":
+#     print(("the quotient is,"),(a/b))
+
+
+#lesson3
+#firstname = ["Chris","Mia","Cloud","Luna"]
+#lastname = ["Ramessar","Franklyn","Totaram"]
+#print(random.choice(firstname),(random.choice(lastname)))
+
+
+#lesson2
+#name=(input("What is your name?"))
+#age=int(input("How old are you?"))
+#print("my name is {} and I am {} years old".format(name,age))
