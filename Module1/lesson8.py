3e46cf9f30ab839a71403e8a757baeb6ec00ccba
Cramessar <ramessar40@gmail.com>
2024-11-10 01:49:41 -0500

    Update lesson8.py

--- Module1/lesson8.py
+++ Module1/lesson8.py
@@ -1,6 +1,7 @@
 #lesson7
 
 list_of_numbers = [3, 99, 12, 1, 7]
+
 # in order to operate on an existing list, i.e to modify a list you need you 
 # use that list as an argument in the function. so new list using existing list 
 # define new list like def newlist(oldlist): 
